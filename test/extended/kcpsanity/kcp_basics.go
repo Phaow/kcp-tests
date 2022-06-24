7b206c057734188ee974a2160570f82fe35026f5
Penghao <pewang@redhat.com>
2022-06-24 18:14:46 +0800

    Fix couldn't get Kcpsanity test suit

--- test/extended/kcpsanity/kcp_basics.go
+++ test/extended/kcpsanity/kcp_basics.go
@@ -1,24 +1,23 @@
 package kcpsanity
 
 import (
-       g "github.com/onsi/ginkgo"
-       o "github.com/onsi/gomega"
-       "k8s.io/apimachinery/pkg/util/wait"
-	e2e "k8s.io/kubernetes/test/e2e/framework"
+	g "github.com/onsi/ginkgo"
+	o "github.com/onsi/gomega"
 
-        exutil "github.com/kcp-dev/kcp-tests/test/extended/util"
+	exutil "github.com/kcp-dev/kcp-tests/test/extended/util"
 )
 
-var _ = g.Describe("[kcp] Kcpsanity", func() {
-        defer g.GinkgoRecover()
+var _ = g.Describe("[sig-kcp] Kcpsanity", func() {
+	defer g.GinkgoRecover()
 
-        var (
-              oc = exutil.NewCLI("kubectl", exutil.KubeConfigPath())
-        )
+	var (
+		oc = exutil.NewCLI("kubectl", exutil.KubeConfigPath())
+	)
 
-        g.It("Author:knarra-Medium-2800712-Checking oc version show clean as gitTreeState value", func() {
-		out, err := kubectl.Run("version").Args("-o", "json").Output()
+	g.It("Author:knarra-Medium-2800712-Checking oc version show clean as gitTreeState value", func() {
+		oc = exutil.NewCLI("kubectl", exutil.KubeConfigPath())
+		_, err := oc.Run("version").Args("-o", "json").Output()
 		o.Expect(err).NotTo(o.HaveOccurred())
 
 	})
-}
+})
